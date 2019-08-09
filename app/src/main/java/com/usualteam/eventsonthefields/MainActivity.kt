4077951299b0c6d1f9117e52da00053ae4295341
egorbaranov <enterskylife@gmail.com>
2019-08-10 00:34:03 +0300

    Initial commit

---
+++ app/src/main/java/com/usualteam/eventsonthefields/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.usualteam.eventsonthefields
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    // BottomNavigationView: Dashboard + Log
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
